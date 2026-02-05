653a7d5483e4ac6980b6433c113d21ca6d0720bd
cheese-and-nachos <102952541+cheese-and-nachos@users.noreply.github.com>
2026-02-05 10:35:56 +0100

    Added property to return associated grid into ScalarField class.

--- dojo/field.py
+++ dojo/field.py
@@ -3,4 +3,20 @@ class ScalarField1D:
       on a one-dimensional domain (i.e. grid)"""
 
     def __init__(self, grid, values):
-        pass
+        self._grid = grid
+        self._values = values
+        self._validate_attributes()
+
+    def _validate_attributes(self):
+        if self._grid.size != self._values.size:
+            raise ValueError("values must have same size as grid")
+
+    @property
+    def grid(self):
+        return self._grid
+
+    @grid.setter
+    def grid(self, newgrid):
+        self._grid = newgrid
+        self._validate_attributes()
+
--- test/test_field.py
+++ test/test_field.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pytest
 
 from dojo.field import ScalarField1D
 from dojo.grid import Grid1D
@@ -14,3 +15,51 @@ def test_scalarfield1d_initialisation():
 
     # assert
     assert scalar is not None
+
+def test_scalarfield1d_size_values_grid():
+    #arrange
+    grid = Grid1D(0, 1, 100)
+    values = np.linspace(0, 10, 98)
+
+    #act
+    #scalar = ScalarField1D(grid, values)
+
+    #assert
+    with pytest.raises(ValueError):
+        scalar = ScalarField1D(grid, values)
+
+def test_scalarfield1d_returngrid():
+    #arrange
+    grid = Grid1D(0, 1, 100)
+    values = np.linspace(0, 10, 100)
+
+    #act
+    scalar = ScalarField1D(grid, values)
+    actual = scalar.grid
+
+    #assert
+    assert grid == actual
+
+def test_scalarfield1d_gridproperty():
+    #arrange
+    grid = Grid1D(0, 1, 100)
+    values = np.linspace(0, 10, 100)
+    scalar = ScalarField1D(grid, values)
+
+
+    #assert
+    with pytest.raises(AttributeError):
+        scalar.grid = 10
+
+def test_scalarfield1d_reassigngrid():
+    #arrange
+    grid1 = Grid1D(0, 1, 100)
+    grid2 = Grid1D(1, 2, 100)
+    values = np.linspace(0, 10, 100)
+    scalar = ScalarField1D(grid1, values)
+
+    #act
+    scalar.grid = grid2
+
+    #assert
+    assert scalar.grid is grid2
